b41dfc2af109d8b23ea0db4adbff91dcb82a6e13
Ignytis <155588001+ignytis@users.noreply.github.com>
2024-07-07 00:38:54 +0300

    Python initializes the environment once

--- torustiq-transform-python/src/lib.rs
+++ torustiq-transform-python/src/lib.rs
@@ -1,16 +1,31 @@
 mod py_record;
 
-use std::{collections::HashMap, sync::Mutex};
+use std::{
+    collections::HashMap, sync::{mpsc::{channel, Sender}, Mutex}, thread, time::Duration
+};
 
 use log::debug;
 use once_cell::sync::Lazy;
 use pyo3::prelude::*;
 
 use torustiq_common::{
-    ffi::{shared::get_param, types::{module::{ModuleInfo, ModuleProcessRecordFnResult, ModuleStepHandle, ModuleStepInitArgs, ModuleStepInitFnResult, PipelineStepKind, Record}, std_types::ConstCStrPtr}},
+    ffi::{
+        shared::get_param,
+        types::{
+            module::{
+                ModuleInfo, ModuleProcessRecordFnResult, ModuleStepHandle, ModuleStepInitArgs,
+                ModuleStepInitFnResult, PipelineStepKind, Record
+            },
+            std_types::ConstCStrPtr
+        }},
     logging::init_logger};
 
-static MODULE_INIT_ARGS: Lazy<Mutex<HashMap<ModuleStepHandle, ModuleStepInitArgs>>> = Lazy::new(|| {
+struct ModuleStepAtributes {
+    init_args: ModuleStepInitArgs,
+    sender: Sender<Record>,
+}
+
+static MODULE_INIT_ARGS: Lazy<Mutex<HashMap<ModuleStepHandle, ModuleStepAtributes>>> = Lazy::new(|| {
     Mutex::new(HashMap::new())
 });
 
@@ -37,35 +52,55 @@ extern "C" fn torustiq_module_step_init(args: ModuleStepInitArgs) -> ModuleStepI
         return ModuleStepInitFnResult::ErrorKindNotSupported;
     }
 
-    MODULE_INIT_ARGS.lock().unwrap().insert(args.step_handle, args);
+    let (tx, rx) = channel::<Record>();
+    // TODO: add a parameter to read a Python file. File is preferrable place for larger code
+    let code = get_param(args.step_handle, "code_contents").unwrap_or(String::from(""));
+    // In order not to re-initialize the Python environment on each data processing,
+    // the data is received in a loop inside a thread
+    thread::spawn(move|| {
+        Python::with_gil(|py| {
+            let module: Bound<PyModule> = PyModule::from_code_bound(
+                py,
+                &code,
+                "torustiq_module_process_record.py",
+                "torustiq_module_process_record",
+            ).unwrap();
+            module.add_class::<py_record::PyRecord>().unwrap();
+
+            loop {
+                let in_record: py_record::PyRecord = match rx.recv_timeout(Duration::from_secs(1)) {
+                    Ok(r) => r,
+                    Err(_) => continue, // timeout
+                }.into();
+
+                let out_record: py_record::PyRecord = module.getattr("process").unwrap()
+                    .call1((in_record,)).unwrap()
+                    .extract().unwrap();
+                let out_record: Record = out_record.into();
+
+                let on_data_received_fn = match MODULE_INIT_ARGS.lock().unwrap().get(&args.step_handle) {
+                    Some(m) => m.init_args.on_data_received_fn,
+                    None => return ModuleProcessRecordFnResult::None,
+                };
+                on_data_received_fn(out_record, args.step_handle);
+            }
+        });
+    });
+
+    MODULE_INIT_ARGS.lock().unwrap().insert(args.step_handle, ModuleStepAtributes {
+        init_args: args,
+        sender: tx,
+    });
     ModuleStepInitFnResult::Ok
 }
 
 #[no_mangle]
 extern "C" fn torustiq_module_process_record(in_record: Record, step_handle: ModuleStepHandle) -> ModuleProcessRecordFnResult {
-    let code = get_param(step_handle, "code_contents").unwrap_or(String::from(""));
-    let new_record = Python::with_gil(|py| {
-        let py_record: py_record::PyRecord = in_record.into();
-        let module: Bound<PyModule> = PyModule::from_code_bound(
-            py,
-            &code,
-            "torustiq_module_process_record.py",
-            "torustiq_module_process_record",
-        ).unwrap();
-        module.add_class::<py_record::PyRecord>().unwrap();
-
-        let out_record: py_record::PyRecord = module.getattr("process").unwrap()
-            .call1((py_record,)).unwrap()
-            .extract().unwrap();
-
-        out_record.into()
-    });
-
-    let on_data_received_fn = match MODULE_INIT_ARGS.lock().unwrap().get(&step_handle) {
-        Some(m) => m.on_data_received_fn,
+    let mutex = MODULE_INIT_ARGS.lock().unwrap();
+    let sender = match mutex.get(&step_handle) {
+        Some(m) => &m.sender,
         None => return ModuleProcessRecordFnResult::None,
     };
-    on_data_received_fn(new_record, step_handle);
-
+    sender.send(in_record).unwrap();
     ModuleProcessRecordFnResult::None
 }
