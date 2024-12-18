2061d35dba639fce0036a3967266ea1e770a7395
Ignytis <155588001+ignytis@users.noreply.github.com>
2024-12-18 23:57:41 +0200

    Prometheus listener - configuration

--- prometheus/src/lib.rs
+++ prometheus/src/lib.rs
@@ -1,8 +1,11 @@
+use log::debug;
 use metrics_exporter_prometheus::PrometheusBuilder;
+use std::{collections::HashMap, net::SocketAddrV4};
+
 use torustiq_common::{
     ffi::{
-        shared::set_listener_module_configuration,
-        types::module as module_types,  
+        shared::{get_params, set_listener_module_configuration},
+        types::module as module_types, utils::strings::string_to_cchar,
     },
     logging::init_logger,
     CURRENT_API_VERSION,
@@ -14,6 +17,9 @@ const MODULE_INFO: module_types::ModuleInfo = module_types::ModuleInfo {
     kind: module_types::ModuleKind::Listener,
     name: c"Prometheus metrics".as_ptr(),
 };
+
+const DEFAULT_LISTEN_HOST: &str = "0.0.0.0";
+const DEFAULT_LISTEN_PORT: &str = "9000";
 
 #[no_mangle]
 pub extern "C" fn torustiq_module_get_info() -> module_types::ModuleInfo {
@@ -33,9 +39,27 @@ extern "C" fn torustiq_module_listener_configure(args: module_types::ModuleListe
 }
 
 #[no_mangle]
-extern "C" fn torustiq_module_common_start(_handle: module_types::ModuleHandle) -> module_types::StepStartFnResult {
-    let builder = PrometheusBuilder::new();
-    builder.install().expect("failed to install recorder/exporter");
+extern "C" fn torustiq_module_common_start(handle: module_types::ModuleHandle) -> module_types::StepStartFnResult {
+    debug!("Starting...");
+    let params = get_params(handle).unwrap_or(HashMap::new());
+    let host = params.get("listen.host").map(String::clone).unwrap_or(DEFAULT_LISTEN_HOST.into());
+    let port = params.get("port").map(String::clone).unwrap_or(DEFAULT_LISTEN_PORT.into());
+    let sock_addr_str = format!("{}:{}", host, port);
+    let sock_addr: SocketAddrV4 = match sock_addr_str.parse() {
+        Ok(a) => a,
+        Err(e) => return module_types::StepStartFnResult::ErrorMisc(
+            string_to_cchar(format!("Failed to format an endpoint address from host '{}' and port '{}': {}", host, port, e)))
+    };
+    let builder = PrometheusBuilder::new()
+        .with_http_listener(sock_addr);
+    if let Err(e) = builder.install() {
+        return module_types::StepStartFnResult::ErrorMisc(
+            string_to_cchar(format!("Cannot listen a socket on host '{}' and port '{}': {}", host, port, e)));
+    }
+    debug!("Started Prometheus metrics server on '{}'.", sock_addr_str);
+    metrics::gauge!("count_test").set(0.0);
+
+    metrics::gauge!("count_test").increment(10.0);
 
     module_types::StepStartFnResult::Ok
 }
